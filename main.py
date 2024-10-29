bab238f31db40711b1f028b62a529dbefc70f127
anton <andrutskyi.anton@lll.kpi.ua>
2024-10-29 12:56:24 +0200

    v 0.2 (rewrite record)

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
+import random
+import string
+
 AMOUNT_OF_BLOCKS = 10
 LENGTH_OF_BLOCK = 1000
+INDEX_LENGTH = 5
+STR_LENGTH = 20
+LINE_LENGTH = 30
+INDEX_LINE_LENGTH = 13
 DATA_FILE = "data_file.txt"
 INDEX_FILE = "index_file"
 
 
-class Record:
-    def __init__(self, is_deleted:int, index:int, data:str):
-        self.is_deleted = is_deleted
-        self.index = index
-        self.data = data
+# ASSIST FUNCTIONS
+def hash_func(index):
+    return index // LENGTH_OF_BLOCK
+
+
+def generate_random_string(length=STR_LENGTH):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def line_modify(record):
+    line = str(record.is_deleted) + ";"
+    str_index = str(record.index)
+    while len(str(str_index)) < INDEX_LENGTH:
+        str_index = "_" + str_index
+    line += str_index + ";" + record.data + "\n"
+    return line
+
+
+def index_line_modify(index, data_index):
+    str_index = str(index)
+    while len(str(str_index)) < INDEX_LENGTH:
+        str_index = "_" + str_index
+
+    str_data_index = str(data_index)
+    while len(str(str_data_index)) < INDEX_LENGTH:
+        str_data_index = "_" + str_data_index
+
+    return str_index + ";" + str_data_index + "\n"
+
+
+def get_index(line_to_compare:str):
+    index_line = int(line_to_compare.split(";")[0].strip('_'))
+    return index_line
 
 
 def create_files():
@@ -17,42 +52,44 @@ def create_files():
         f.close()
 
 
-def hash_func(index):
-    return index // LENGTH_OF_BLOCK
-
-
-def get_index(line_to_compare:str):
-    index_line = int(line_to_compare.split(";")[1].strip())
-    return index_line
+# MAIN FUNCTIONS
+class Record:
+    def __init__(self, is_deleted:int, index:int):
+        self.is_deleted = is_deleted
+        self.index = index
+        self.data = generate_random_string()
 
 
 def add_record(record):
     f = open(DATA_FILE, "a")
-    f.write(str(record.is_deleted) + ';' + str(record.index) + ';' + record.data + '\n')
+    f.write(line_modify(record))
+    data_line_index = f.tell() // LINE_LENGTH
     f.close()
 
-    hash_result = hash_func(record.index)
-    f = open(INDEX_FILE + str(hash_result) + ".txt", "r")
-    new_record_pos_found = False
-    current_pos = 0
-    lines = []
-    for line in f:
-        lines.append(line)
+    index = hash_func(record.index)
+    f_index = open(INDEX_FILE + str(index) + ".txt", "r")
+    f_index.seek(0,2)
+    f_index_length = f_index.tell() // INDEX_LINE_LENGTH
 
-        if not new_record_pos_found:
-            line_index = get_index(line)
-            current_pos += 1
-            if record.index < line_index:
-                current_pos -= 1
-                new_record_pos_found = True
-    f.close()
+    left, right = 0, f_index_length
+    while left < right:
+        mid = left + (right - left) // 2
+        f_index.seek(mid * INDEX_LINE_LENGTH)
+        line = f_index.readline()
+        if get_index(line) < record.index:
+            left = mid + 1  # Move to the right half
+        else:
+            right = mid  # Move to the left half
+    insert_index = left
+    f_index.close()
 
-    lines.insert(current_pos, str(record.is_deleted) + ';' + str(record.index) + ';' + record.data + '\n')
-    f = open(INDEX_FILE + str(hash_result) + ".txt", "w")
-    f.writelines(lines)
-    f.close()
+    file = open(INDEX_FILE + str(index) + ".txt", "r+")
+    file.seek(insert_index * INDEX_LINE_LENGTH)
+    lines_after_insert = file.read()
+    file.seek(insert_index * INDEX_LINE_LENGTH)
+    file.write(index_line_modify(record.index, data_line_index) + lines_after_insert)
+    file.close()
 
 
-
-rec1 = Record(is_deleted=0, index=2001, data="ABOBA")
+rec1 = Record(is_deleted=0, index=6)
 add_record(rec1)
