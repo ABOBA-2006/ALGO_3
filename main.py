80e4d153fbd376248ff09acaf858af3e7ccf02a7
anton <andrutskyi.anton@lll.kpi.ua>
2024-10-29 14:44:35 +0200

    v 0.3 (edit record)

--- main.py
+++ main.py
@@ -41,6 +41,15 @@ def index_line_modify(index, data_index):
     return str_index + ";" + str_data_index + "\n"
 
 
+def data_modify(data):
+    if len(data) > STR_LENGTH:
+        return data[:STR_LENGTH]
+    elif len(data) < STR_LENGTH:
+        while len(data) < STR_LENGTH:
+            data = data + '_'
+    return data
+
+
 def get_index(line_to_compare:str):
     index_line = int(line_to_compare.split(";")[0].strip('_'))
     return index_line
@@ -60,6 +69,33 @@ class Record:
         self.data = generate_random_string()
 
 
+def binary_search_to_insert(left, right, file, index):
+    while left < right:
+        mid = left + (right - left) // 2
+        file.seek(mid * INDEX_LINE_LENGTH)
+        line = file.readline()
+        if get_index(line) < index:
+            left = mid + 1  # Move to the right half
+        else:
+            right = mid  # Move to the left half
+    return left
+
+
+def binary_search(file, left, right, index):
+    while left <= right:
+        mid = left + (right - left) // 2  # Calculate the midpoint
+        file.seek(mid * INDEX_LINE_LENGTH)
+        line = file.readline()
+        index_of_line = get_index(line)
+        if index_of_line == index:
+            return mid  # Target found
+        elif index_of_line < index:
+            left = mid + 1  # Discard left half
+        else:
+            right = mid - 1  # Discard right half
+    return -1  # Target not found
+
+
 def add_record(record):
     f = open(DATA_FILE, "a")
     f.write(line_modify(record))
@@ -67,29 +103,39 @@ def add_record(record):
     f.close()
 
     index = hash_func(record.index)
-    f_index = open(INDEX_FILE + str(index) + ".txt", "r")
+    f_index = open(INDEX_FILE + str(index) + ".txt", "r+")
     f_index.seek(0,2)
     f_index_length = f_index.tell() // INDEX_LINE_LENGTH
 
-    left, right = 0, f_index_length
-    while left < right:
-        mid = left + (right - left) // 2
-        f_index.seek(mid * INDEX_LINE_LENGTH)
-        line = f_index.readline()
-        if get_index(line) < record.index:
-            left = mid + 1  # Move to the right half
-        else:
-            right = mid  # Move to the left half
-    insert_index = left
+    insert_index = binary_search_to_insert(0, f_index_length, f_index, record.index)
+
+    f_index.seek(insert_index * INDEX_LINE_LENGTH)
+    lines_after_insert = f_index.read()
+    f_index.seek(insert_index * INDEX_LINE_LENGTH)
+    f_index.write(index_line_modify(record.index, data_line_index) + lines_after_insert)
     f_index.close()
 
-    file = open(INDEX_FILE + str(index) + ".txt", "r+")
-    file.seek(insert_index * INDEX_LINE_LENGTH)
-    lines_after_insert = file.read()
-    file.seek(insert_index * INDEX_LINE_LENGTH)
-    file.write(index_line_modify(record.index, data_line_index) + lines_after_insert)
-    file.close()
 
+def edit_record(record, data):
+    index = hash_func(record.index)
+    file_search = open(INDEX_FILE + str(index) + ".txt", "r")
+    file_search.seek(0, 2)
+    f_index_length = file_search.tell() // INDEX_LINE_LENGTH
 
-rec1 = Record(is_deleted=0, index=6)
-add_record(rec1)
+    editing_index = binary_search(file_search, 0, f_index_length, record.index)
+
+    file_search.seek(editing_index * INDEX_LINE_LENGTH)
+    editing_line = file_search.readline()
+    editing_line_data_index = int(editing_line.split(';')[1].strip('_')) - 1
+    file_search.close()
+
+    file_edit = open(DATA_FILE, "r+")
+    file_edit.seek(editing_line_data_index * LINE_LENGTH)
+    lines_edit = file_edit.readlines()
+    lines_edit[0] = ';'.join(line_to_edit := lines_edit[0].split(';')[:2] + [data_modify(data)]) + '\n'
+    file_edit.seek(editing_line_data_index * LINE_LENGTH)
+    file_edit.writelines(lines_edit)
+    file_edit.close()
+
+rec1 = Record(is_deleted=0, index=4)
+edit_record(rec1, 'POPA')
